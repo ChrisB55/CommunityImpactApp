66861a630a75841f7b87da36792eadc92eed047c
Christopher W. Brown <christopherwilliambrown@gmail.com>
2017-04-17 14:10:42 -0400

    Add working googlemap
    One button for zoom to next project.

--- app/components/mapSearch.jsx
+++ app/components/mapSearch.jsx
@@ -1,20 +1,51 @@
+
 var React = require('react');
+
 var Message = require('Message');
-var GMap = require('WeatherMap');
+var WeatherMap = require('WeatherMap');
+const PROJECT_POSITION = {
+  lat: 38.98942,
+  lng: -77.03731
+};
 
-var MapSearch = React.createClass({
+const STARTING_POSITION = {
+  lat: 38.88942,
+  lng: -77.03731
+};
+
+
+var MapSearch = class Map extends React.Component {
+  constructor() {
+    super();
+    this.panToProject = this.panToProject.bind(this);
+  }
+  
+  componentDidMount() {
+    this.map = new google.maps.Map(this.refs.map, {
+      center: STARTING_POSITION,
+      zoom: 16
+    });
+  }
+  
+  panToProject() {
+    console.log(this)
+    this.map.panTo(PROJECT_POSITION);
+  }
+  
   render() {
-  return (
-    <div>
-      <h1 className="text-center page-title">Search here</h1>
-      <p>
-         Map Search
-         <input type="search" placeholder="Use the Map" ref="search"/>
-      </p>
-    </div>
-  )
-  },
-});
-
+    const mapStyle = {
+      width: 500,
+      height: 300,
+      border: '1px solid black'
+    };
+    
+    return (
+      <div>
+        <button onClick={this.panToProject}>First Local Project</button>
+        <div ref="map" style={mapStyle}>A Map of Washington DC</div>
+      </div>
+    );
+  }
+};
 
 module.exports = MapSearch;
